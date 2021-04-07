9dd28707c360e5a044ebb96a671f9bf6e71a159d
Giuseppe Napoli <giuseppe.napoli1993@hotmail.it>
2021-04-07 17:51:53 +0200

    Finito

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,22 @@
+/* section chilometri */
 var km_percorsi = parseInt(prompt ('Quanti chilometri percorrerai?'));
 if(isNaN(km_percorsi)){
   alert('Non hai inserito correttamente i chilometri, aggiorna la pagine e riprova')
-}
-var price = 0.21;
-var prz_km = km_percorsi * price;
-var prz_20 = ((prz_km * 20) / 100);
-var prz_40 = ((prz_km * 40) / 100);
+} //alert
 
+var price = 0.21; /* prezzo biglietto per chilometro */
+var prz_km = km_percorsi * price;
+var prz_20 = ((prz_km * 20) / 100); /* prezzo scontato del 20% */
+var prz_40 = ((prz_km * 40) / 100); /* prezzo scontato del 40% */
+
+/* section età passeggeri */
 var età = parseInt(prompt ('Età passegero?'));
 if(isNaN(età)){
   alert('Non hai inserito correttamente i tuoi anni, aggiorna la pagine e riprova')
-}
+} //alert
+
 var sconto = prz_km;
+/* condizioni per ricevere lo sconto */
 if(età < 18){
   var sconto = prz_km - prz_20;
   document.getElementById('fascia').innerHTML = "*In quanto minorenne il prezzo è stato scontato del 20%"; 
@@ -25,5 +30,6 @@ if(età < 18){
   console.log(sconto);
 }
 
+/* section messaggio su HTML */
 document.getElementById('passegero').innerHTML = "Gentile Passegero, il prezzo previsto per il suo viaggio e di " + sconto.toFixed(2) + "€"; 
 
