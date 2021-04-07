476d8241030580525424eae9501251e0ff27c880
Giuseppe Napoli <giuseppe.napoli1993@hotmail.it>
2021-04-07 17:58:09 +0200

    ora sono serio, finito!

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var prz_20 = ((prz_km * 20) / 100); /* prezzo scontato del 20% */
 var prz_40 = ((prz_km * 40) / 100); /* prezzo scontato del 40% */
 
 /* section età passeggeri */
-var età = parseInt(prompt ('Età passegero?'));
+var età = parseInt(prompt ('Età passeggero?'));
 if(isNaN(età)){
   alert('Non hai inserito correttamente i tuoi anni, aggiorna la pagine e riprova')
 } //alert
@@ -19,11 +19,11 @@ var sconto = prz_km;
 /* condizioni per ricevere lo sconto */
 if(età < 18){
   var sconto = prz_km - prz_20;
-  document.getElementById('fascia').innerHTML = "*In quanto minorenne il prezzo è stato scontato del 20%"; 
+  document.getElementById('fascia').innerHTML = "*In quanto minorenne il prezzo è stato scontato del 20%."; 
   console.log(sconto);
 }else if(età > 65){
   var sconto = prz_km - prz_40;
-  document.getElementById('fascia').innerHTML = "*In quanto over 65 il prezzo è stato scontato del 40%"; 
+  document.getElementById('fascia').innerHTML = "*In quanto over 65 il prezzo è stato scontato del 40%."; 
   console.log(sconto);
 }else{
   var sconto = prz_km;
@@ -31,5 +31,5 @@ if(età < 18){
 }
 
 /* section messaggio su HTML */
-document.getElementById('passegero').innerHTML = "Gentile Passegero, il prezzo previsto per il suo viaggio e di " + sconto.toFixed(2) + "€"; 
+document.getElementById('passeggero').innerHTML = "Gentile Passeggero, il prezzo previsto per il suo viaggio è di " + sconto.toFixed(2) + "€"; 
 
